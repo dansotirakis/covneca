b326831516d2e855e7258613c1884e5c75731275
Damianos Sotirakis <dansotirakis18@gmail.com>
2021-09-27 03:35:19 -0300

    feat(CTB01) : fix settings file variable not work

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 BASE_API_URL = os.environ["BASE_API_URL"]
-BISCOINT = os.environ["BISCOINT_API_URL"]
 PHOEMUR = os.environ["FUNDAMENTUS"]
 COINLIB = os.environ['COINLIB']
 OKANE = os.environ["OKANE"]
