88ef9879a76c986c698aa19eeabf4519d8b93957
Damianos Sotirakis <dansotirakis18@gmail.com>
2021-09-27 02:40:25 -0300

    feat(CTB01) : add settings ref env

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
-BASE_API_URL = os.environ("BASE_API_URL")
-BISCOINT = os.environ("BISCOINT_API_URL")
-PHOEMUR = os.environ("FUNDAMENTUS")
+BASE_API_URL = os.environ["BASE_API_URL"]
+BISCOINT = os.environ["BISCOINT_API_URL"]
+PHOEMUR = os.environ["FUNDAMENTUS"]
 COINLIB = os.environ['COINLIB']
-OKANE = os.environ("OKANE")
+OKANE = os.environ["OKANE"]
